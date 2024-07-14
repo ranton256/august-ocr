0a395ac8da2b04ba72a98942b8c2b02b3e6172fb
Richard Anton <ranton@ranton.org>
2024-07-14 11:24:57 -0700

    Switched to conda, added preprocessing of images

--- viewer_app.py
+++ viewer_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import difflib
+from pathlib import Path
+from text_from_pdfs import get_preproc_path
 
 st.set_page_config(
     page_title="August OCR",
@@ -13,25 +16,37 @@ def main():
     df = pd.read_csv("output/results.csv")
     st.title("OCR Comparison App")
 
-    page = st.slider('Select Page', 1, len(df), 1)
+    if len(df) > 1:
+        page = st.slider('Select Page', 1, len(df), 1)
+    else:
+        page = 1
+
     st.write(f"Showing page {page}")
 
     image_path = df.loc[page - 1, 'image_path']
     extracted_text = df.loc[page - 1, 'extracted']
     corrected_text = df.loc[page - 1, 'corrected']
 
-    image = Image.open(image_path)
+    output_dir = "output"
 
-    col1, col2, col3 = st.columns(3)
+    image = Image.open(image_path)
+    pre_path = get_preproc_path(image_path, output_dir)
+    pre_image = Image.open(pre_path)
+
+
+    col1, col2, col3, col4 = st.columns(4)
 
     with col1:
         st.image(image, caption=f'Page {page}', use_column_width=True)
 
     with col2:
+        st.image(pre_image, caption=f'Preprocessed Page {page}', use_column_width=True)
+
+    with col3:
         st.subheader("Extracted Text")
         st.write(extracted_text)
 
-    with col3:
+    with col4:
         st.subheader("Corrected Text")
         st.write(corrected_text)
 
