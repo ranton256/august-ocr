dc3bdfa3569f10c6c71be960806c5372353cdeb4
Richard Anton <ranton@ranton.org>
2024-09-18 14:32:36 -0700

    Improve layout and page controls

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
 import os
 
+
 def get_preproc_path(path, output_dir):
     path_base, path_ext = os.path.splitext(os.path.basename(path))
     preproc_file_name = path_base + "_proc.jpg"
     return os.path.join(output_dir, preproc_file_name)
-
